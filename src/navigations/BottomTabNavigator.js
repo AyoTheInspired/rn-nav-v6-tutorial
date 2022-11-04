6fa385328aec2b25b6c6939a36ae6d86f2c433db
Ayo <ayoinspired@gmail.com>
2022-11-04 08:09:42 +0100

    Added settings tab navigator

--- src/navigations/BottomTabNavigator.js
+++ src/navigations/BottomTabNavigator.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Home, Notifications, Settings, Wallet} from '../screens';
-import {ROUTES} from '../constants';
+import {COLORS, ROUTES} from '../constants';
 import Icon from 'react-native-vector-icons/Ionicons';
+import SettingsNavigator from './SettingsNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,18 +12,34 @@ export default function BottomTabNavigator() {
     <Tab.Navigator
       screenOptions={({route}) => ({
         headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: COLORS.primary,
         tabBarIcon: ({color, size, focused}) => {
           let iconName;
-
           if (route.name === ROUTES.HOME_TAB) {
-            return focused ? 'home' : 'home-outline';
+            iconName = focused ? 'ios-home-sharp' : 'ios-home-outline';
+          } else if (route.name === ROUTES.SETTINGS_NAVIGATOR) {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === ROUTES.WALLET) {
+            iconName = focused ? 'wallet' : 'wallet-outline';
+          } else if (route.name === ROUTES.NOTIFICATIONS) {
+            iconName = focused
+              ? 'md-notifications-sharp'
+              : 'md-notifications-outline';
           }
+          return <Icon name={iconName} size={22} color={color} />;
         },
       })}>
       <Tab.Screen name={ROUTES.HOME_TAB} component={Home} />
       <Tab.Screen name={ROUTES.WALLET} component={Wallet} />
       <Tab.Screen name={ROUTES.NOTIFICATIONS} component={Notifications} />
-      <Tab.Screen name={ROUTES.SETTINGS} component={Settings} />
+      <Tab.Screen
+        name={ROUTES.SETTINGS_NAVIGATOR}
+        component={SettingsNavigator}
+        options={{
+          tabBarLabel: 'Settings',
+        }}
+      />
     </Tab.Navigator>
   );
 }
