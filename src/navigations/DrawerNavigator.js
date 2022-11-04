4b87b6e49a72c7a6dbe3b7664aa53f682e3688f8
Ayo <ayoinspired@gmail.com>
2022-11-04 08:48:59 +0100

    Styled drawer navigator

--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -1,6 +1,7 @@
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import React from 'react';
-import {ROUTES} from '../constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {COLORS, ROUTES} from '../constants';
 import {Home, Notifications, Wallet} from '../screens';
 import BottomTabNavigator from './BottomTabNavigator';
 
@@ -8,12 +9,46 @@ const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name={ROUTES.HOME_TAB} component={BottomTabNavigator} />
-      <Drawer.Screen name={ROUTES.WALLET_DRAWER} component={Wallet} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: COLORS.primary,
+        drawerActiveTintColor: COLORS.white,
+        drawerLabelStyle: {
+          marginLeft: -20,
+        },
+      }}>
+      <Drawer.Screen
+        name={ROUTES.HOME_DRAWER}
+        component={BottomTabNavigator}
+        options={{
+          title: 'Home',
+          drawerIcon: ({focused, color, size}) => (
+            <Icon name="home-sharp" size={18} color={color} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name={ROUTES.WALLET_DRAWER}
+        component={Wallet}
+        options={{
+          title: 'Wallet',
+          drawerIcon: ({focused, color, size}) => (
+            <Icon name="wallet" size={18} color={color} />
+          ),
+        }}
+      />
+
       <Drawer.Screen
         name={ROUTES.NOTIFICATIONS_DRAWER}
         component={Notifications}
+        options={{
+          title: 'Notifications',
+          drawerIcon: ({focused, color, size}) => (
+            <Icon name="notifications" size={18} color={color} />
+          ),
+        }}
       />
     </Drawer.Navigator>
   );
